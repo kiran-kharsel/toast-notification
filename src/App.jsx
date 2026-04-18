e1c0bd5fde4539c8808270b4443ee8ce64e83313
kiran-kharsel <kirankharsel@gmail.com>
2026-04-18 16:11:40 +0530

    style: add style to toast-component and close-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import ToastNotification from './toast-notification/ToastNotification'
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       
-      
+      <ToastNotification/>
     </>
   )
 }
