cde9ca9de57b584d1263f0f77ba98d6e86343715
kiran-kharsel <kirankharsel@gmail.com>
2026-04-19 19:49:07 +0530

    feat: recieve props and add data in elements

--- src/toast-notification/ToastNotification.jsx
+++ src/toast-notification/ToastNotification.jsx
@@ -5,19 +5,26 @@ import infoIcon from '../assets/information.png'
 import successIcon from '../assets/success.png'
 import warningIcon from '../assets/warning.png'
 
-function ToastNotification() {
+function ToastNotification(props) {
+    const {id = '',title='', desc='', onRemove = ()=>{}, cta ='', type = 'info'} = props;
+
+
+    function handleRemove(){
+        onRemove(id)
+    }
+
   return (
     <div className='toast'>
-        <button className='toast-close'>&times;</button>
+        <button onClick={handleRemove} className='toast-close'>&times;</button>
         <div className="toast-content">
             <div className="toast-info">
                 <img className='info-icon' height={'24px'} width={'24px'} src={infoIcon} alt="" />
                 <div className="toast-title-desc">
-                    <span>title</span>
-                    <span>desc</span>
+                    <span>{title}</span>
+                    <span>{desc}</span>
                 </div>
             </div>
-            <div className="toast-cta"></div>
+            <div className="toast-cta">{cta}</div>
         </div>
         <div className="toast-progress"></div>
     </div>
